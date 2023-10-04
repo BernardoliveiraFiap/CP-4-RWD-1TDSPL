bea25e9b9d60fa4b183eb2d052b055d47b010eb0
MatheusFelipecamarinhaDuarte <matheus.camarinha.duarte@gmail.com>
2023-10-03 22:14:35 -0300

    Feito o excluir produtos com sucesso

--- src/routes/ExcluirProdutos.jsx
+++ src/routes/ExcluirProdutos.jsx
@@ -1,6 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { ListaProdutos } from "../components/ListaProdutos";
 import style from "./ExcluirProdutos.module.css";
+import { useEffect, useState } from "react";
 
 export default function ExcluirProdutos() {
   document.title = "Excluir Produtos";
@@ -9,18 +9,48 @@ export default function ExcluirProdutos() {
 
   //Receber o ID do produto pelo HOOK useParams( );
   const { id } = useParams();
-
   //Recuperar o produto na lista pelo ID.
-  const produto = ListaProdutos.filter((produto) => produto.id == id)[0];
+  
+  const [listaProdutoExterno, setListaProdutoExterno] = useState([{
 
+  }]);
+
+
+  // console.log(produto)
+  useEffect(() => {
+    console.log("eu estou aqui")
+    fetch("http://localhost:5000/produtos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => setListaProdutoExterno(data))
+    .catch((error) => console.log(error));
+  }, []);
+  
+  const produto = listaProdutoExterno.filter((produto) => produto.id == id);
+  // const produto = produtoSemLista[0]
+  console.log(produto, id)
+  
   const handleDelete = (event) => {
     event.preventDefault();
+    fetch(`http://localhost:5000/produtos/${id}`,{
+      method:"DELETE",
+      headers:{
+        "Content-Type":"application/json"
+      },
+    })
+    .then((response)=> response.json())
+    .then((response)=> console.log(response))
+    .catch(error=> console.log(error));
 
     let indice;
 
-    indice = ListaProdutos.findIndex((item) => item.id === produto.id);
+    indice = listaProdutoExterno.findIndex((item) => item.id === produto.id);
 
-    ListaProdutos.splice(indice, 1);
+    listaProdutoExterno.splice(indice, 1);
 
     alert("Produto exclído com sucesso!");
 
@@ -34,10 +64,12 @@ export default function ExcluirProdutos() {
         
         <div className={style.card}>
             <h2>Produto Selecionado</h2>
+            {produto.map((produto) =>(
             <figure>
                 <img src={produto.img} alt={produto.desc} title={produto.desc}/>
                 <figcaption>{produto.nome} - <span>R$ </span>{produto.preco}</figcaption>
             </figure>
+            ))}
             <div className={style.btn}>
                 <button onClick={handleDelete}>EXCLUIR</button>
                 <button onClick={()=> navigate("/produtos")}>CANCELAR</button>
